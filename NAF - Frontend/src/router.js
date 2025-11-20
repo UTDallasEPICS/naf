a11267bab7ad875c0b5fd29f4973afa0645a9520
Dshah1003 <sdrashti.10jan@gmail.com>
2025-11-20 12:24:46 -0600

    Added frontend project folder

---
+++ NAF - Frontend/src/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+import SystemControl from "./pages/SystemControl.vue";
+import DatabaseViewer from "./pages/DatabaseViewer.vue";
+
+const routes = [
+  { path: "/", redirect: "/system-control" },
+  { path: "/system-control", component: SystemControl },
+  { path: "/database-viewer", component: DatabaseViewer },
+];
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
